3e1c9d3f12a3e8c36f671c31d4512af775d5892d
amitshuklabag <amitshuklabag@gmail.com>
2022-04-13 14:05:03 +0530

    Sample repo created

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from "./elestio-logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" />
+        <p>Elestio React Example</p>
+        <a
+          className="App-link"
+          href="https://elest.io/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Go to App{" "}
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
